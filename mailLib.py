f649090e5bcabfc2b6abaaddb2dd42f30e8744fa
aboxer <aboxer51@yahoo.com>
2020-02-03 17:40:47 -0500

    gmail class added and works

--- mailLib.py
+++ mailLib.py
@@ -12,35 +12,10 @@ import json
 import time
 import datetime
 
+###################### stuff related to gmail model class ###############################
 # If modifying these scopes, delete the file token.pickle.
 #SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
 SCOPES = ['https://www.googleapis.com/auth/gmail.send']
-
-def create_service():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('gmail', 'v1', credentials=creds)
-    return service
 
 def create_message(sender, to, subject, message_text):
   """Create a message for an email.
@@ -65,25 +40,56 @@ def create_message(sender, to, subject, message_text):
   #return {'raw': base64.urlsafe_b64encode(message.as_bytes())}
 
 
-def send_message(service, user_id, message):
-  """Send an email message.
 
-  Args:
-    service: Authorized Gmail API service instance.
-    user_id: User's email address. The special value "me"
-    can be used to indicate the authenticated user.
-    message: Message to be sent.
+class gmailr:
+  def __init__(self):
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
-  Returns:
-    Sent Message.
-  """
-  try:
-    message = (service.users().messages().send(userId=user_id, body=message)
-               .execute())
-    print('Message Id: %s' % message['id'])
-    return message
-  except HttpError as error:
-    print('An error occurred: %s' % error)
+    self.service = build('gmail', 'v1', credentials=creds)
+
+
+  def send_message(self, msgList):
+    """Send an email message.
+
+    Args:
+      service: Authorized Gmail API service instance.
+      user_id: User's email address. The special value "me"
+      can be used to indicate the authenticated user.
+      message: Message to be sent.
+
+    Returns:
+      Sent Message.
+    """
+    dnList = []
+    for msg in msgList:
+      try:
+        message = (self.service.users().messages().send(userId="me", body=msg[1]).execute())
+        #print('Message Id: %s' % message['id'])
+        dnList.append((msg[0],int(time.time())))
+        #return message
+      except HttpError as error:
+        print('An error occurred: %s' % error)
+    return dnList
 
 ###################### stuff related to database model class ###############################
 
@@ -192,6 +198,35 @@ class emailDb:
         if email == rec['email'] and 'lastGv' not in rec:  #this email has never given
           self.db.pop(i)
           break
+
+
+  #get request list
+  def getRqs(self):
+    sendList = []
+    for i in range(len(self.db)):
+      rec = self.db[i]
+      if 'lastRq' not in rec:
+        sendList.append(i)
+
+    msgList = []
+    for i in sendList:
+      rec = self.db[i]
+      email = rec['email']
+      firstNm = rec['fullNm'].split()[0]   #just first name
+      body = firstNm + ' - test message profile.pmc.org/AB0492'
+      msg = create_message('aaron.boxer@gmail.com',email,'test',body)
+      msgList.append((i,msg))
+    return msgList
+
+  def updRqs(self,updList):
+    for upd in updList:
+      idx = upd[0]
+      rec = self.db[idx]
+      self.db[idx]['lastRq'] = upd[1]
+      if 'rqCt' not in rec:
+        self.db[idx]['rqCt'] = 1
+      else:
+        self.db[idx]['rqCt'] += 1
 
 
 #srv = create_service()
--- sendRq.py
+++ sendRq.py
@@ -1,38 +1,10 @@
-import sys
-import json
 import mailLib
-import time
 
-emailDb = 'data/emailDb.json'
-dbf = open(emailDb, 'r')
-r = dbf.read()  #read in all the bytes into one string
-db = json.loads(r)
+ed = mailLib.emailDb('data/emailDb.json')
+gm = mailLib.gmailr()
 
-sendList = []
-for i in range(len(db)):
-  rec = db[i]
-  if 'lastRq' not in rec:
-    sendList.append(i)
-
-for i in sendList:
-  rec = db[i]
-  email = rec['email']
-  firstNm = rec['fullNm'].split()[0]   #just first name
-  body = firstNm + ' - test message profile.pmc.org/AB0492'
-  try:
-    sys.argv[1]
-    srv = mailLib.create_service()
-    msg = mailLib.create_message('aaron.boxer@gmail.com',email,'test',body)
-    mailLib.send_message(srv,'me',msg)
-    db[i]['lastRq'] = int(time.time())
-    if 'rqCt' not in rec:
-      db[i]['rqCt'] = 1
-    else:
-      db[i]['rqCt'] += 1
-  except:
-    print(email)
-
-mailLib.prStats(db)
-
-outf = open(emailDb,'w') #rewrite the database
-json.dump(db,outf)
+rqList = ed.getRqs()
+dnList = gm.send_message(rqList)
+ed.updRqs(dnList)
+ed.prStats()
+ed.exitDb()
